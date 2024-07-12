6a8c8c546082ea0da69bc82b916426f928bfa72a
veikka-p <Veikka.puolitaival@gmail.com>
2024-07-12 13:20:47 +0300

    Update nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -4,6 +4,7 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
+vim.keymap.set('n', '<leader>w', [[:w<CR>]], { noremap = true, silent = true })
 vim.keymap.set('n', '<leader>x', ':Explore<CR>', { silent = true })
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = false
