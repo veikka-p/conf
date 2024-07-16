1f0590fa0a69d5cf392da0c44a242c1c6ce8bc83
Veikka <Veikka.puolitaival@gmail.com>
2024-07-16 19:01:12 +0300

    update nvim conf

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -2,7 +2,21 @@ return {
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
 	opts = {
-		ensure_installed = { "bash", "c", "html", "lua", "luadoc", "markdown", "vim", "vimdoc" },
+		ensure_installed = {
+			"c",
+			"cpp",
+			"lua",
+			"python",
+			"tsx",
+			"javascript",
+			"typescript",
+			"vimdoc",
+			"vim",
+			"bash",
+			"prisma",
+			"astro",
+			"templ",
+		},
 		auto_install = true,
 		highlight = {
 			enable = true,
